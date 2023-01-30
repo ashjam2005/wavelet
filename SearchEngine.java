f977f92e3c9ef4f5f9decd6cf89bc373036e1096
ashjam2005 <122492177+ashjam2005@users.noreply.github.com>
2023-01-30 06:55:26 +0400

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -5,9 +5,11 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-    StringBuilder builder = "";
+    ArrayList<String> list = new ArrayList<String>;
+    StringBuilder builder = new StringBuilder();
     int num2 = 0;
 
+    /*
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Asherja: %d", num);
@@ -49,6 +51,27 @@ class Handler implements URLHandler {
         }
     }
 }
+*/
+    public String handleRequest(URI url)
+    {
+        if (url.getPath.equals("/add-message")
+            {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s"))
+                {
+                    list.add(parameters[1]);
+                }
+                builder.append(list[0]);
+                for (int i=1;i<list.length;i++)
+                {
+                    builder.append("\n");
+                    builder.append(parameters[i])
+                }                
+                return String.format(builder.toString());
+            }
+            return "404 Not Found!";                                 
+    }
+    
 
 class SearchEngine {
     public static void main(String[] args) throws IOException {
